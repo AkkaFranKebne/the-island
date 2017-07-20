236f14a89de25e9b7d3a5a04a7e00e480de4960a
AkkaFranKebne <ewawrobel@gmail.com>
2017-07-20 12:11:34 +0200

    beautify

--- index.php
+++ index.php
@@ -2,20 +2,35 @@
 session_start();
 if(empty($_SESSION["seen_intro"]))$_SESSION["seen_intro"]=0;
 ?>
+
 <!DOCTYPE html>
-<html lang="pl-PL">
-<head>
-    <title>LemOnHills - Agencja Marketingowa</title>
-    <?php include 'elements_meta.php';?>
-</head>
-<body id="main-page-body" <?php if ($_SESSION[ "seen_intro"]==0) { echo "class='loading'"; } ?>>
-    <div id="menuOpacity"> </div>
-    <?php include 'elements_nav_main.php';?>
-    <main id="main-page-main" <?php if ($_SESSION[ "seen_intro"]==0) { echo "class='hidden'"; $_SESSION[ "seen_intro"]=1; }; if ($_SERVER[ 'REQUEST_METHOD']==='GET' ) { if (isset($_GET[ 'source'])===true) { $source=$ _GET[ 'source']; echo "data-source ='$source'"; } }; ?> >
+<html  lang="pl-PL">
+    <head>
+        <title>LemOnHills - Agencja Marketingowa</title>
+        <?php include 'elements_meta.php';?>
+    </head>     
+    <body  id="main-page-body"  <?php   
+       if ($_SESSION["seen_intro"]==0) {
+           echo "class='loading'";
+       }  ?>>
+        <div id="menuOpacity"> </div> 
+        <?php include 'elements_nav_main.php';?>
+        <main id="main-page-main" <?php  
+          if ($_SESSION["seen_intro"]==0) {
+              echo "class='hidden'"; 
+              $_SESSION["seen_intro"]=1; 
+          };
+          if ($_SERVER['REQUEST_METHOD'] ==='GET') {
+            if (isset($_GET['source'])===true) {
+            $source = $_GET['source'];
+            echo "data-source ='$source'";
+                }
+          };
+          ?>  >
         <?php include 'elements_map.php';?>
         <?php include 'elements_island_parts.php';?>
-    </main>
-    <noscript>Strona wymaga uruchomionego Java Script. Zaktualizuj lub zmień przeglądarkę.</noscript>
-    <?php include 'elements_footer.php';?>
-</body>
+        </main> 
+        <noscript>Strona wymaga uruchomionego Java Script. Zaktualizuj lub zmień przeglądarkę.</noscript>    
+        <?php include 'elements_footer.php';?>
+    </body>
 </html>
