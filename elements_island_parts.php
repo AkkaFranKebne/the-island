f653df0dd6d713a569d1c2133e651b64f7abcca7
AkkaFranKebne <ewawrobel@gmail.com>
2017-08-25 12:35:18 +0200

    names in ins from sql

--- elements_island_parts.php
+++ elements_island_parts.php
@@ -3,27 +3,37 @@
 <div class="grpelem hidden" id="wholeisland">
     <img class="block" id="wholeisland_img" src="images/wyspa%20www.png" alt="" />
 </div>
-<div class='pin eventy hidden' title='eventy' href="eventy.php"><p>EVENTY</p></div>
+<div class='pin eventy hidden' title='eventy' <?php $link = "eventy.php"; $text = include 'elements_pin_names.php'; echo ' href="'.$link.'"><p>'.$text .'</p> '?></div>
 <!--
 <div class='pulse eventy hidden' title='eventy' href="eventy.php"></div>
 -->
-<div class='pin social-media hidden' title='social-media' href="social-media.php"><p>SOCIAL MEDIA</p></div>
+<div class='pin social-media hidden' title='social-media' <?php $link = "social-media.php"; $text = include 'elements_pin_names.php'; echo 'href="'.$link.'"><p>'.$text .'</p> '?></div>
 <!--
 <div class='pulse social-media hidden' title='social-media' href="social-media.php"></div>
 -->
-<div class='pin www hidden' title='www' href="www.php"><p>STRONY WWW</p></div>
+<div class='pin www hidden' title='www' <?php $link = "www.php"; $text = include 'elements_pin_names.php'; echo ' href="'.$link.'"><p>'.$text .'</p> '?></div>
 <!--
 <div class='pulse www hidden' title='www' href="www.php"></div>
 -->
-<div class='pin kreacja hidden' title='kreacja' href="kreacja.php"><p>KREACJA</p></div>
+<div class='pin kreacja hidden' title='kreacja' <?php $link = "kreacja.php"; $text = include 'elements_pin_names.php'; echo ' href="'.$link.'"><p>'.$text .'</p> '?></div>
 <!--
 <div class='pulse kreacja hidden' title='kreacja' href="kreacja.php"></div>
 -->
-<div class='pin o-nas hidden' title='o-nas' href="o-nas.php"><p>O NAS</p></div>
+<div class='pin o-nas hidden' title='o-nas' <?php $link = "o-nas.php"; $text = include 'elements_pin_names.php'; echo ' href="'.$link.'"><p>'.$text .'</p> '?></div>
 <!--
 <div class='pulse o-nas hidden' title='o-nas' href="o-nas.php"></div>
 -->
-<div class='pin kontakt hidden' title='kontakt' href="kontakt.php"><p>KONTAKT</p></div>
+<div class='pin kontakt hidden' title='kontakt' <?php $link = "kontakt.php"; $text = include 'elements_pin_names.php'; echo ' href="'.$link.'"><p>'.$text .'</p> '?></div>
 <!--
 <div class='pulse kontakt hidden' title='kontakt' href="kontakt.php"></div>
 -->
+
+
+
+
+
+
+    
+    
+    
+    
